f1d7f4ce6fff0d56262c6affcad7f17d88daef8b
RukhsarAhmed <rukhsar101095@gmail.com>
2025-10-28 14:24:08 +0530

    Modified vite config

--- vite.config.ts
+++ vite.config.ts
@@ -37,6 +37,11 @@
 //   },
 // });
 
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 
 import { defineConfig } from "vite";
